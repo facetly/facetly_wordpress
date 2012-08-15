447eb6a25ea2ca2404cb6059b02e755645399aed
andreas <andreaswijaya89@yahoo.com>
2012-08-15 15:37:32 +0700

    update facetly

--- facetly_admin.php
+++ facetly_admin.php
@@ -1,138 +1,138 @@
 <?php
-	function facetly_admin(){
-		if( !empty($_POST['facetly_settings_hidden']) && $_POST['facetly_settings_hidden'] == 'Y' ) {  
-			$consumer_key = $_POST['facetly_key'];  
-			$consumer_secret = $_POST['facetly_secret'];  
-			$server = $_POST['facetly_server']; 
-			$limit = $_POST['facetly_limit'];
-			$add_variable = $_POST['facetly_add_variable']; 
+function facetly_admin(){
+	if( !empty($_POST['facetly_settings_hidden']) && $_POST['facetly_settings_hidden'] == 'Y' ) {  
+		$consumer_key = $_POST['facetly_key'];  
+		$consumer_secret = $_POST['facetly_secret'];  
+		$server = $_POST['facetly_server']; 
+		$limit = $_POST['facetly_limit'];
+		$add_variable = $_POST['facetly_add_variable']; 
 
-		    try {
-				$facetly = facetly_api_init();
-			    $facetly->setServer($server);
-			    $facetly->setConsumer($consumer_key, $consumer_secret);
-			    $fields = $facetly->fieldSelect();
-		    } catch (Exception $e) {
-		    	$error = $e->getMessage();
-		    }
+	    try {
+			$facetly = facetly_api_init();
+		    $facetly->setServer($server);
+		    $facetly->setConsumer($consumer_key, $consumer_secret);
+		    $fields = $facetly->fieldSelect();
+	    } catch (Exception $e) {
+	    	$error = $e->getMessage();
+	    }
 
-		    if (empty($fields)) {
-		    	echo '<div class="error"><p><strong>Can not connect to server, please check your consumer API configuration or contact our support if problem persist.</strong></p></div>';
-		    } else {
-				$settings = array(
-					'key' => $consumer_key,
-					'secret' => $consumer_secret,
-					'server' => $server,
-					'limit' => $limit,
-					'add_variable' => $add_variable,
-				);
+	    if (empty($fields)) {
+	    	echo '<div class="error"><p><strong>Can not connect to server, please check your consumer API configuration or contact our support if problem persist.</strong></p></div>';
+	    } else {
+			$settings = array(
+				'key' => $consumer_key,
+				'secret' => $consumer_secret,
+				'server' => $server,
+				'limit' => $limit,
+				'add_variable' => $add_variable,
+			);
 
-				update_option('facetly_settings', $settings);  
-				?>  
-				<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
-				<?php  
-		    }
-		} else {  
-			$common = get_option('facetly_settings');
-			$consumer_key = trim($common['key']);
-			$consumer_secret = trim($common['secret']);
-			$server = $common['server'];
-			$limit = $common['limit'];
-			$add_variable = $common['add_variable'];
-		}
-
-		if( !empty($_POST['facetly_copy_hidden']) && $_POST['facetly_copy_hidden'] == 'Y' ) {
-			if ( is_writable(TEMPLATEPATH) ) {
-				if( file_exists(TEMPLATEPATH."/searchform.php") ) {
-					$zipfilename = "searchform.php";
-					$zipsource = TEMPLATEPATH. "/searchform.php";
-					$zipdest = TEMPLATEPATH. "/";
-					$backup = zipfile($zipfilename, $zipsource, $zipdest);
-					unlink(TEMPLATEPATH. "/searchform.php");
-				} else {
-					$backup = true;
-				}
-
-				$unzipsource = WP_PLUGIN_DIR. "/facetly-woocommerce/facetly-search-template.zip";
-				$unzipdest = TEMPLATEPATH. "/";  //folder directory must be ended with "/", example: c:/xampp/htdocs/wordpress/
-				$unzip1 = unzipfile($unzipsource, $unzipdest);
-		
-				if ( $backup && $unzip1 ) {
-					$facetly_page = get_page_by_path('finds');;
-					$facetly_page_id = $facetly_page->ID;
-					update_post_meta($facetly_page_id, "_wp_page_template", "facetly-search-template.php");
-					echo "<h4>" . __( 'Files Copy Success' ) . "</h4>";
-				} else {
-					echo "<h4>" . __( 'Files Copy Not Success' ) . "</h4>";
-				}
-			} else {
-				echo "<h4>" . __( 'Theme Folder is Not Writable' ) . "</h4>";
-			}
-		}
-	?> 
-
-	<div class="wrap">  
-		<?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
-		<?php echo "<h4>" . __( 'Facetly API Configuration' ) . "</h4>"; ?>  
-		<form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
-			<input type="hidden" name="facetly_settings_hidden" value="Y">  
-			<table>
-				<tr>
-					<td><?php _e("Consumer Key"); ?></td>
-					<td><?php _e(":");?></td>
-					<td><input type="text" name="facetly_key" value="<?php echo $consumer_key; ?>" size="50"><?php _e(" ex: qhduafdh" ); ?></td>
-				</tr>
-				<tr>
-					<td><?php _e("Consumer Secret"); ?></td>
-					<td><?php _e(":");?></td>
-					<td><input type="text" name="facetly_secret" value="<?php echo $consumer_secret; ?>" size="50"><?php _e(" ex: q5yvmddqntukobeoszi6zuqmwvy9wwsv" ); ?></td>
-				</tr>
-				<tr>
-					<td><?php _e("Server Name" ); ?></td>
-					<td><?php _e(":");?></td>
-					<td><input type="text" name="facetly_server" value="<?php echo $server; ?>" size="50"><?php _e(" ex: http://us1.beta.facetly.com/1" ); ?></td>
-				</tr>
-				<tr>
-					<td><?php _e("Search Limit Setting" ); ?></td>
-					<td><?php _e(":");?></td>
-					<td><input type="text" name="facetly_limit" value="<?php echo $limit; ?>" size="50"><?php _e(" ex: 5" ); ?></td>
-				</tr>
-
-				<tr>
-					<td><?php _e("Additional Variable" ); ?></td>
-					<td><?php _e(":");?></td>
-					<td><input type="text" name="facetly_add_variable" value="<?php echo $add_variable; ?>" size="50"><?php _e(" ex: _op[category]=or" ); ?></td>
-				</tr>
-				<tr>
-					<td>
-						<p class="submit">  
-							<input type="submit" name="Submit" value="<?php _e('Submit Data' ) ?>" />  
-						</p>  
-					</td>
-				</tr>
-			</table>
-		</form>  
-
-		<?php
-			if ( file_exists(TEMPLATEPATH."/facetly-search-template.php") && file_exists(TEMPLATEPATH."/searchform.php") ) {
-				echo "<div class='custom_notice'><p><strong>File Already Exist</strong></p></div>";
-			}
-		?>
-		<form name="facetly_copy" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
-			<input type="hidden" name="facetly_copy_hidden" value="Y">  
-			<table>
-				<tr>
-					<td><?php echo "<h4>" . __( 'Copy facetly_search_template.php and searchform.php to your current active theme' ) . "</h4>"; ?>  </td>
-				</tr>
-				<tr>
-					<td>
-						<p class="submit">  
-							<input type="submit" name="Submit" value="<?php _e('Copy File' ) ?>" />  
-						</p>  
-					</td>
-				</tr>
-			</table>
-		</form>  
-	</div> 
-<?php
+			update_option('facetly_settings', $settings);  
+			?>  
+			<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
+			<?php  
+	    }
+	} else {  
+		$common = get_option('facetly_settings');
+		$consumer_key = trim($common['key']);
+		$consumer_secret = trim($common['secret']);
+		$server = $common['server'];
+		$limit = $common['limit'];
+		$add_variable = $common['add_variable'];
 	}
+
+	if( !empty($_POST['facetly_copy_hidden']) && $_POST['facetly_copy_hidden'] == 'Y' ) {
+		if ( is_writable(TEMPLATEPATH) ) {
+			if( file_exists(TEMPLATEPATH."/searchform.php") ) {
+				$zipfilename = "searchform.php";
+				$zipsource = TEMPLATEPATH. "/searchform.php";
+				$zipdest = TEMPLATEPATH. "/";
+				$backup = zipfile($zipfilename, $zipsource, $zipdest);
+				unlink(TEMPLATEPATH. "/searchform.php");
+			} else {
+				$backup = true;
+			}
+
+			$unzipsource = WP_PLUGIN_DIR. "/facetly-woocommerce/facetly-search-template.zip";
+			$unzipdest = TEMPLATEPATH. "/";  //folder directory must be ended with "/", example: c:/xampp/htdocs/wordpress/
+			$unzip1 = unzipfile($unzipsource, $unzipdest);
+	
+			if ( $backup && $unzip1 ) {
+				$facetly_page = get_page_by_path('finds');;
+				$facetly_page_id = $facetly_page->ID;
+				update_post_meta($facetly_page_id, "_wp_page_template", "facetly-search-template.php");
+				echo "<h4>" . __( 'Files Copy Success' ) . "</h4>";
+			} else {
+				echo "<h4>" . __( 'Files Copy Not Success' ) . "</h4>";
+			}
+		} else {
+			echo "<h4>" . __( 'Theme Folder is Not Writable' ) . "</h4>";
+		}
+	}
+?> 
+
+<div class="wrap">  
+	<?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
+	<?php echo "<h4>" . __( 'Facetly API Configuration' ) . "</h4>"; ?>  
+	<form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+		<input type="hidden" name="facetly_settings_hidden" value="Y">  
+		<table>
+			<tr>
+				<td><?php _e("Consumer Key"); ?></td>
+				<td><?php _e(":");?></td>
+				<td><input type="text" name="facetly_key" value="<?php echo $consumer_key; ?>" size="50"><?php _e(" ex: qhduafdh" ); ?></td>
+			</tr>
+			<tr>
+				<td><?php _e("Consumer Secret"); ?></td>
+				<td><?php _e(":");?></td>
+				<td><input type="text" name="facetly_secret" value="<?php echo $consumer_secret; ?>" size="50"><?php _e(" ex: q5yvmddqntukobeoszi6zuqmwvy9wwsv" ); ?></td>
+			</tr>
+			<tr>
+				<td><?php _e("Server Name" ); ?></td>
+				<td><?php _e(":");?></td>
+				<td><input type="text" name="facetly_server" value="<?php echo $server; ?>" size="50"><?php _e(" ex: http://us1.beta.facetly.com/1" ); ?></td>
+			</tr>
+			<tr>
+				<td><?php _e("Search Limit Setting" ); ?></td>
+				<td><?php _e(":");?></td>
+				<td><input type="text" name="facetly_limit" value="<?php echo $limit; ?>" size="50"><?php _e(" ex: 5" ); ?></td>
+			</tr>
+
+			<tr>
+				<td><?php _e("Additional Variable" ); ?></td>
+				<td><?php _e(":");?></td>
+				<td><input type="text" name="facetly_add_variable" value="<?php echo $add_variable; ?>" size="50"><?php _e(" ex: _op[category]=or" ); ?></td>
+			</tr>
+			<tr>
+				<td>
+					<p class="submit">  
+						<input type="submit" name="Submit" value="<?php _e('Submit Data' ) ?>" />  
+					</p>  
+				</td>
+			</tr>
+		</table>
+	</form>  
+
+	<?php
+		if ( file_exists(TEMPLATEPATH."/facetly-search-template.php") && file_exists(TEMPLATEPATH."/searchform.php") ) {
+			echo "<div class='custom_notice'><p><strong>File Already Exist</strong></p></div>";
+		}
+	?>
+	<form name="facetly_copy" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+		<input type="hidden" name="facetly_copy_hidden" value="Y">  
+		<table>
+			<tr>
+				<td><?php echo "<h4>" . __( 'Copy facetly_search_template.php and searchform.php to your current active theme' ) . "</h4>"; ?>  </td>
+			</tr>
+			<tr>
+				<td>
+					<p class="submit">  
+						<input type="submit" name="Submit" value="<?php _e('Copy File' ) ?>" />  
+					</p>  
+				</td>
+			</tr>
+		</table>
+	</form>  
+</div> 
+<?php
+}
--- facetly_common.php
+++ facetly_common.php
@@ -1,189 +1,188 @@
 <?php
 
-	function facetly_api_init() {
-	    static $facetly;
+function facetly_api_init() {
+    static $facetly;
 
-	    if ( empty( $facetly ) ) {
-	        require_once("facetly_api.php");
+    if ( empty( $facetly ) ) {
+        require_once("facetly_api.php");
 
-	        $facetly = new facetly_api();
-	        $common = get_option('facetly_settings');
-			$consumer_key = $common['key'];
-			$consumer_secret = $common['secret'];
-			$server = $common['server'];
-			$add_variable = $common['add_variable'];
-			$base_url = "/finds?". $add_variable;
+        $facetly = new facetly_api();
+        $common = get_option('facetly_settings');
+		$consumer_key = $common['key'];
+		$consumer_secret = $common['secret'];
+		$server = $common['server'];
+		$add_variable = $common['add_variable'];
+		$base_url = "/finds?". $add_variable;
 
-			$facetly->setConsumer($consumer_key, $consumer_secret); 
-			$facetly->setServer($server);
-			$facetly->setBaseUrl($base_url);
-	    }
-	    return $facetly;
-	}
+		$facetly->setConsumer($consumer_key, $consumer_secret); 
+		$facetly->setServer($server);
+		$facetly->setBaseUrl($base_url);
+    }
+    return $facetly;
+}
 
-	function custom_get_child($parent_id, $terms_childs, $tax) {
-		foreach ($terms_childs as $key => $value) {
-			if($value->parent == $parent_id) {
-				$parent_id = $value->term_id;
-				$name = $value->name;
-				$tax[] = $name;
-				unset($terms_childs[$parent_id]);
-				$taxo = custom_get_child($parent_id, $terms_childs, $tax);
-				
-				return $taxo;
-			}
-		}
-		return $tax;
-	}
-
-	function custom_taxonomies_terms_links($post_id) {
-		//generate only taxonomy related to post
-		$terms = wp_get_object_terms( $post_id, 'wpsc_product_category', array('orderby' => 'parent', 'order' => 'DESC', 'fields' => 'all') );
-
-		foreach ($terms as $key => $value) {
-			$terms_childs[$value->term_id] = $value;
-		}
-	
-		foreach ($terms as $key => $value) {
-			$terms_parents[$value->parent][$value->term_id] = $value;
-		}
-
-		$parents = $terms_parents[0];
-		foreach ($parents as $key => $value) {
-			$parent_id = $value->term_id;
-			unset($terms_childs[$parent_id]);
-		}
-		//print_r($terms_childs);
-		foreach ($parents as $key => $value) {
+function custom_get_child($parent_id, $terms_childs, $tax) {
+	foreach ($terms_childs as $key => $value) {
+		if($value->parent == $parent_id) {
 			$parent_id = $value->term_id;
 			$name = $value->name;
-			$tax = array();
 			$tax[] = $name;
 			unset($terms_childs[$parent_id]);
-
-			$taxonomy[] = custom_get_child($parent_id, $terms_childs, $tax);
+			$taxo = custom_get_child($parent_id, $terms_childs, $tax);
+			
+			return $taxo;
 		}
+	}
+	return $tax;
+}
 
-		if (!empty($taxonomy)) {
-			foreach ($taxonomy as $key => $value) {
-				$cat[] = join(';', $value);
+function custom_taxonomies_terms_links($post_id) {
+	//generate only taxonomy related to post
+	$terms = wp_get_object_terms( $post_id, 'wpsc_product_category', array('orderby' => 'parent', 'order' => 'DESC', 'fields' => 'all') );
+
+	foreach ($terms as $key => $value) {
+		$terms_childs[$value->term_id] = $value;
+	}
+
+	foreach ($terms as $key => $value) {
+		$terms_parents[$value->parent][$value->term_id] = $value;
+	}
+
+	$parents = $terms_parents[0];
+	foreach ($parents as $key => $value) {
+		$parent_id = $value->term_id;
+		unset($terms_childs[$parent_id]);
+	}
+	foreach ($parents as $key => $value) {
+		$parent_id = $value->term_id;
+		$name = $value->name;
+		$tax = array();
+		$tax[] = $name;
+		unset($terms_childs[$parent_id]);
+
+		$taxonomy[] = custom_get_child($parent_id, $terms_childs, $tax);
+	}
+
+	if (!empty($taxonomy)) {
+		foreach ($taxonomy as $key => $value) {
+			$cat[] = join(';', $value);
+		}
+	}
+
+	return $cat;
+}
+
+function zipfile($filename, $pathsource, $pathdestination){
+	$pathsource = str_replace("\\", "/", $pathsource);
+	$pathdestination = str_replace("\\", "/", $pathdestination);
+
+	$fp = fopen($pathsource, 'r');
+	$filecontent = fread($fp, filesize($pathsource));
+	fclose($fp);
+
+	$zip = new ZipArchive();
+	$filezip =  $filename.".zip";
+
+	$compress = $zip->open($pathdestination. $filezip, ZIPARCHIVE::CREATE);
+	if ($compress) {
+	   $zip->addFromString( $filename, $filecontent);
+	   $zip->close();
+
+	   return true;
+	} else {
+		return false;
+	}
+}
+
+function unzipfile($pathsource, $pathdestination){
+	$pathsource = str_replace("\\", "/", $pathsource);
+	$pathdestination = str_replace("\\", "/", $pathdestination);
+
+	$zip = zip_open($pathsource);
+	if ($zip) {
+		while ($zip_entry = zip_read($zip)) {
+			$fp = fopen($pathdestination. zip_entry_name($zip_entry), "w");
+			if (zip_entry_open($zip, $zip_entry, "r")) {
+		  		$buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+		  		fwrite($fp,"$buf");
+				
+		 		zip_entry_close($zip_entry);
+				fclose($fp);
 			}
-		}
-
-		return $cat;
+	  	}
+		zip_close($zip);
+		return true;
+	} else {
+		return false;
 	}
+}
 
-	function zipfile($filename, $pathsource, $pathdestination){
-		$pathsource = str_replace("\\", "/", $pathsource);
-		$pathdestination = str_replace("\\", "/", $pathdestination);
+function facetly_save_post($post_id) {
+	$post = get_post($post_id);
+	$post_type = $post->post_type;
+	$post_status = $post->post_status;
+	
+	if($post_status == 'trash' || $post_status == 'auto-draft' || $post_type != 'wpsc-product'){
+		return $post_id;
+	} else if ($post_status == 'publish' && $post_type == 'wpsc-product') {
+		try {
+			facetly_insert_product($post_id);
+		} catch (Exception $e) {
+			echo '
+				<tr>
+					<td colspan="11" class="custom_error">'. $e->getMessage(). '</td>
+				</tr>
+			';
+		}
+	} else if ( ($post_status == 'pending' || $post_status == 'draft' ) && $post_type == 'wpsc-product') {
+		$facetly->productDelete($post_id);
+	}
+}
 
-		$fp = fopen($pathsource, 'r');
-		$filecontent = fread($fp, filesize($pathsource));
-		fclose($fp);
+function facetly_insert_product($post_id){
+	global $wpdb;
 
-		$zip = new ZipArchive();
-		$filezip =  $filename.".zip";
+	$post = get_post($post_id);
+	$url = get_permalink($post_id);
+	$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-size' );
+	$imageurl = $image['0'];
+	
+	$category = custom_taxonomies_terms_links($post_id);
+	$item = array();
+	$meta = get_post_meta($post_id, '');
 
-		$compress = $zip->open($pathdestination. $filezip, ZIPARCHIVE::CREATE);
-		if ($compress) {
-		   $zip->addFromString( $filename, $filecontent);
-		   $zip->close();
-
-		   return true;
-		} else {
-			return false;
+	$facetly_fields = get_option('facetly_fields');
+	foreach( $facetly_fields as $key => $value ) {
+		if ( strstr($value, 'post') ) {	
+			$item[$key] = $post->$value;
+		} else if (isset($meta[$value])) {
+			$item[$key] = $meta[$value][0];
 		}
 	}
+	$item['id'] = $post_id;
+	$item['url'] = $url;
+	$item['imageurl'] = $imageurl;
+	$item['category'] = $category;
+	$date = new DateTime($item['created']);
+	$item['created'] = $date->getTimestamp() *1000;
+	
+	$facetly = facetly_api_init();
+	$facetly->productUpdate($item);
+}
+add_action('wp_insert_post', 'facetly_save_post');
 
-	function unzipfile($pathsource, $pathdestination){
-		$pathsource = str_replace("\\", "/", $pathsource);
-		$pathdestination = str_replace("\\", "/", $pathdestination);
-
-		$zip = zip_open($pathsource);
-		if ($zip) {
-			while ($zip_entry = zip_read($zip)) {
-				$fp = fopen($pathdestination. zip_entry_name($zip_entry), "w");
-				if (zip_entry_open($zip, $zip_entry, "r")) {
-			  		$buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-			  		fwrite($fp,"$buf");
-					
-			 		zip_entry_close($zip_entry);
-					fclose($fp);
-				}
-		  	}
-			zip_close($zip);
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	function facetly_save_post($post_id) {
-		$post = get_post($post_id);
-		$post_type = $post->post_type;
-		$post_status = $post->post_status;
-		
-		if($post_status == 'trash' || $post_status == 'auto-draft' || $post_type != 'wpsc-product'){
-			return $post_id;
-		} else if ($post_status == 'publish' && $post_type == 'wpsc-product') {
-			try {
-				facetly_insert_product($post_id);
-			} catch (Exception $e) {
-				echo '
-					<tr>
-						<td colspan="11" class="custom_error">'. $e->getMessage(). '</td>
-					</tr>
-				';
-			}
-		} else if ( ($post_status == 'pending' || $post_status == 'draft' ) && $post_type == 'wpsc-product') {
+function facetly_delete_product($post_id) {
+	$post = get_post($post_id);
+	$post_type = $post->post_type;
+	if($post->post_status == 'trash' or $post->post_status == 'auto-draft' or $post_type != 'product'){
+		return $post_id;
+	} else if ($post_type == 'product') {
+		try {
+			$facetly = facetly_api_init();
 			$facetly->productDelete($post_id);
+		} catch (Exception $e) {
+			echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
 		}
 	}
-
-	function facetly_insert_product($post_id){
-		global $wpdb;
-
-		$post = get_post($post_id);
-		$url = get_permalink($post_id);
-		$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-size' );
-		$imageurl = $image['0'];
-		
-		$category = custom_taxonomies_terms_links($post_id);
-		$item = array();
-		$meta = get_post_meta($post_id, '');
-
-		$facetly_fields = get_option('facetly_fields');
-		foreach( $facetly_fields as $key => $value ) {
-			if ( strstr($value, 'post') ) {	
-				$item[$key] = $post->$value;
-			} else if (isset($meta[$value])) {
-				$item[$key] = $meta[$value][0];
-			}
-		}
-		$item['id'] = $post_id;
-		$item['url'] = $url;
-		$item['imageurl'] = $imageurl;
-		$item['category'] = $category;
-		$date = new DateTime($item['created']);
-		$item['created'] = $date->getTimestamp() *1000;
-		
-		$facetly = facetly_api_init();
-		$facetly->productUpdate($item);
-	}
-	add_action('wp_insert_post', 'facetly_save_post');
-
-	function facetly_delete_product($post_id) {
-		$post = get_post($post_id);
-		$post_type = $post->post_type;
-		if($post->post_status == 'trash' or $post->post_status == 'auto-draft' or $post_type != 'product'){
-			return $post_id;
-		} else if ($post_type == 'product') {
-			try {
-				$facetly = facetly_api_init();
-				$facetly->productDelete($post_id);
-			} catch (Exception $e) {
-				echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
-			}
-		}
-	}
-	add_action('wp_trash_post', 'facetly_delete_product');
+}
+add_action('wp_trash_post', 'facetly_delete_product');
