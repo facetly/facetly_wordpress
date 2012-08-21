ef5ae18831d1685b25a87e5c8529b31c2130430d
pionize <andrew@skyshi.com>
2012-08-21 15:47:21 +0700

    update facetly

--- facetly_conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$facetly = new facetly_api;
-$common = get_option('facetly_settings');
-$consumer_key = $common['key'];
-$consumer_secret = $common['secret'];
-$server = $common['server'];
-$facetly->setConsumer($consumer_key, $consumer_secret); 
-$facetly->setServer($server);
-$facetly->setBaseUrl("/finds");
