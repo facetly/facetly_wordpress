125bb57b12cc405acebf1ba8fc7c39e2aef43dee
pionize <andrew@skyshi.com>
2012-08-27 14:40:14 +0700

    update facetly

--- facetly_template.php
+++ facetly_template.php
@@ -1,6 +1,16 @@
 <?php
     function facetly_template(){
         $facetly = facetly_api_init();
+        try {
+            $template = $facetly->templateSelect();
+            if (empty($tplfacet) && empty($tplsearch)) {
+                $tplsearch = $template->tplsearch;
+                $tplfacet = $template->tplfacet;
+            }
+            $fields = $facetly->fieldSelect();
+        } catch (Exception $e) {
+            echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
+        }
         if( !empty($_POST['facetly_template_hidden']) && $_POST['facetly_template_hidden'] == 'Y' ) {  
             $tplsearch = stripslashes($_POST['tplsearch']);  
             $tplfacet = stripslashes($_POST['tplfacet']);  
@@ -22,46 +32,36 @@
         } else {
             $tplsearch = get_option('facetly_tplsearch');
             $tplfacet = get_option('facetly_tplfacet');
-
-            try {
-                $template = $facetly->templateSelect();
-                if (empty($tplfacet) && empty($tplsearch)) {
-                    $tplsearch = $template->tplsearch;
-                    $tplfacet = $template->tplfacet;
-                }
-            } catch (Exception $e) {
-                echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
-            }
             if (empty($template)) {
                 echo '<div class="error"><p><strong>Can not connect to server, please check your consumer API configuration or contact our support if problem persist.</strong></p></div>';
             }
+        }
+        if (!empty($fields)) {
+        ?> 
+            <div class="wrap">  
+                <?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
+                <?php echo "<h4>" . __( 'Facetly Template' ) . "</h4>"; ?>  
+                <form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+                    <input type="hidden" name="facetly_template_hidden" value="Y">  
+                    <table>
+                        <tr>
+                            <td><?php _e("Search Template:"); ?></td>
+                            <td><textarea name="tplsearch" cols="150" rows="20"><?php echo $tplsearch ?></textarea></td>
+                        </tr>
+                        <tr>
+                            <td><?php _e("Facet Template:"); ?></td>
+                            <td><textarea name="tplfacet" cols="150" rows="20"><?php echo $tplfacet ?></textarea></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p class="submit">  
+                                    <input type="submit" name="Submit" value="<?php _e('Submit Data' ) ?>" />  
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </form>  
+            </div> 
+        <?php
         } 
-        
-    ?> 
-
-    <div class="wrap">  
-        <?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
-        <?php echo "<h4>" . __( 'Facetly Template' ) . "</h4>"; ?>  
-        <form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
-            <input type="hidden" name="facetly_template_hidden" value="Y">  
-            <table>
-                <tr>
-                    <td><?php _e("Search Template:"); ?></td>
-                    <td><textarea name="tplsearch" cols="150" rows="20"><?php echo $tplsearch ?></textarea></td>
-                </tr>
-                <tr>
-                    <td><?php _e("Facet Template:"); ?></td>
-                    <td><textarea name="tplfacet" cols="150" rows="20"><?php echo $tplfacet ?></textarea></td>
-                </tr>
-                <tr>
-                    <td>
-                        <p class="submit">  
-                            <input type="submit" name="Submit" value="<?php _e('Submit Data' ) ?>" />  
-                        </p>
-                    </td>
-                </tr>
-            </table>
-        </form>  
-    </div> 
-    <?php
-}
+    }
