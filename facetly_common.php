7757a3565bf46f10f7aee739f58cc1aaff1747f1
andreas <andreaswijaya89@yahoo.com>
2012-09-05 10:38:59 +0700

    update facetly

--- facetly_common.php
+++ facetly_common.php
@@ -1,197 +1,233 @@
 <?php
 
-	function facetly_api_init() {
-        static $facetly;
-        
-        if ( empty( $facetly ) ) {
-            require_once('facetly_api.php');
-            $common = get_option('facetly_settings');
-            $facetly = new facetly_api();
-            if (!empty($common)) {
-                $consumer_key = $common['key'];
-                $consumer_secret = $common['secret'];
-                $server = $common['server'];
-                $add_variable = $common['add_variable'];
-            } else {
-                $consumer_key = '';
-                $consumer_secret = '';
-                $server = '';
-                $limit = '';
-                $add_variable = '';
-            }
-            $facetly_page_id = get_option('facetly_page_id');
-			$facetly_page_data = get_page($facetly_page_id);
-			$facetly_post_name = $facetly_page_data->post_name;
-            $base_url = '/'.$facetly_post_name. '?'. $add_variable;
-            $facetly->setConsumer($consumer_key, $consumer_secret); 
-            $facetly->setServer($server);
-            $facetly->setBaseUrl($base_url);
-        }
-        return $facetly;
+function facetly_api_init() {
+static $facetly;
+
+if ( empty( $facetly ) ) {
+    require_once('facetly_api.php');
+    $common = get_option('facetly_settings');
+    $facetly = new facetly_api();
+    if (!empty($common)) {
+        $consumer_key = $common['key'];
+        $consumer_secret = $common['secret'];
+        $server = $common['server'];
+        $add_variable = $common['add_variable'];
+    } else {
+        $consumer_key = '';
+        $consumer_secret = '';
+        $server = '';
+        $limit = '';
+        $add_variable = '';
     }
+    $facetly_page_id = get_option('facetly_page_id');
+		$facetly_page_data = get_page($facetly_page_id);
+		$facetly_post_name = $facetly_page_data->post_name;
+    $base_url = '/'.$facetly_post_name. '?'. $add_variable;
+    $facetly->setConsumer($consumer_key, $consumer_secret); 
+    $facetly->setServer($server);
+    $facetly->setBaseUrl($base_url);
+}
+return $facetly;
+}
 
-	function custom_get_child($parent_id, $terms_childs, $tax) {
-		foreach ($terms_childs as $key => $value) {
-			if($value->parent == $parent_id) {
-				$parent_id = $value->term_id;
-				$name = $value->name;
-				$tax[] = $name;
-				unset($terms_childs[$parent_id]);
-				$taxo = custom_get_child($parent_id, $terms_childs, $tax);
-				
-				return $taxo;
-			}
-		}
-		return $tax;
-	}
-
-	function custom_taxonomies_terms_links($post_id) {
-		//generate only taxonomy related to post
-		$terms = wp_get_object_terms( $post_id, 'wpsc_product_category', array('orderby' => 'parent', 'order' => 'DESC', 'fields' => 'all') );
-
-		foreach ($terms as $key => $value) {
-			$terms_childs[$value->term_id] = $value;
-		}
-
-		foreach ($terms as $key => $value) {
-			$terms_parents[$value->parent][$value->term_id] = $value;
-		}
-
-		$parents = $terms_parents[0];
-		foreach ($parents as $key => $value) {
-			$parent_id = $value->term_id;
-			unset($terms_childs[$parent_id]);
-		}
-		foreach ($parents as $key => $value) {
+function custom_get_child($parent_id, $terms_childs, $tax) {
+	foreach ($terms_childs as $key => $value) {
+		if($value->parent == $parent_id) {
 			$parent_id = $value->term_id;
 			$name = $value->name;
-			$tax = array();
 			$tax[] = $name;
 			unset($terms_childs[$parent_id]);
-
-			$taxonomy[] = custom_get_child($parent_id, $terms_childs, $tax);
-		}
-
-		if (!empty($taxonomy)) {
-			foreach ($taxonomy as $key => $value) {
-				$cat[] = join(';', $value);
-			}
-		}
-
-		return $cat;
-	}
-
-	function zipfile($filename, $pathsource, $pathdestination){
-		$pathsource      = str_replace('\\', '/', $pathsource);
-        $pathdestination = str_replace('\\', '/', $pathdestination);
-
-		$fp = fopen($pathsource, 'r');
-		$filecontent = fread($fp, filesize($pathsource));
-		fclose($fp);
-
-		$zip = new ZipArchive();
-		$filezip =  $filename.".zip";
-
-		$compress = $zip->open($pathdestination. $filezip, ZIPARCHIVE::CREATE);
-		if ($compress) {
-		   $zip->addFromString( $filename, $filecontent);
-		   $zip->close();
-
-		   return true;
-		} else {
-			return false;
+			$taxo = custom_get_child($parent_id, $terms_childs, $tax);
+			
+			return $taxo;
 		}
 	}
+	return $tax;
+}
 
-	function unzipfile($pathsource, $pathdestination) {
-        $pathsource      = str_replace('\\', '/', $pathsource);
-        $pathdestination = str_replace('\\', '/', $pathdestination);
-        
-        $zip = zip_open($pathsource);
-        if ($zip) {
-            while ($zip_entry = zip_read($zip)) {
-                $fp = fopen($pathdestination . zip_entry_name($zip_entry), 'w');
-                if (zip_entry_open($zip, $zip_entry, 'r')) {
-                    $buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-                    fwrite($fp, "$buf");
-                    
-                    zip_entry_close($zip_entry);
-                    fclose($fp);
-                }
-            }
-            zip_close($zip);
-            return true;
-        } else {
-            return false;
+function custom_taxonomies_terms_links($post) {
+	$post_id = $post->ID;
+	
+	$post_type = $post->post_type;
+	if (!empty($post->ancestors[0])) {
+		$ancestors = $post->ancestors[0];
+		$post_type = get_post_type($ancestors);
+	}
+	//$cat = $post->post_category;
+	/*print_r($post_type);
+	exit();*/
+	if ($post_type == 'wpsc-product') {
+		$category = 'wpsc_product_category'; 
+	} else if ($post_type == 'post') {
+		$category = 'category';
+	}
+	//generate only taxonomy related to post
+	//$facetly_fields = get_option('facetly_fields');
+	$terms = wp_get_object_terms( $post_id, $category, array('orderby' => 'parent', 'order' => 'DESC', 'fields' => 'all') );
+
+	foreach ($terms as $key => $value) {
+		$terms_childs[$value->term_id] = $value;
+	}
+
+	foreach ($terms as $key => $value) {
+		$terms_parents[$value->parent][$value->term_id] = $value;
+	}
+
+	$parents = $terms_parents[0];
+	foreach ($parents as $key => $value) {
+		$parent_id = $value->term_id;
+		unset($terms_childs[$parent_id]);
+	}
+	foreach ($parents as $key => $value) {
+		$parent_id = $value->term_id;
+		$name = $value->name;
+		$tax = array();
+		$tax[] = $name;
+		unset($terms_childs[$parent_id]);
+
+		$taxonomy[] = custom_get_child($parent_id, $terms_childs, $tax);
+	}
+
+	if (!empty($taxonomy)) {
+		foreach ($taxonomy as $key => $value) {
+			$cat[] = join(';', $value);
+		}
+	}
+
+	return $cat;
+}
+
+function zipfile($filename, $pathsource, $pathdestination){
+	$pathsource      = str_replace('\\', '/', $pathsource);
+$pathdestination = str_replace('\\', '/', $pathdestination);
+
+	$fp = fopen($pathsource, 'r');
+	$filecontent = fread($fp, filesize($pathsource));
+	fclose($fp);
+
+	$zip = new ZipArchive();
+	$filezip =  $filename.".zip";
+
+	$compress = $zip->open($pathdestination. $filezip, ZIPARCHIVE::CREATE);
+	if ($compress) {
+	   $zip->addFromString( $filename, $filecontent);
+	   $zip->close();
+
+	   return true;
+	} else {
+		return false;
+	}
+}
+
+function unzipfile($pathsource, $pathdestination) {
+$pathsource      = str_replace('\\', '/', $pathsource);
+$pathdestination = str_replace('\\', '/', $pathdestination);
+
+$zip = zip_open($pathsource);
+if ($zip) {
+    while ($zip_entry = zip_read($zip)) {
+        $fp = fopen($pathdestination . zip_entry_name($zip_entry), 'w');
+        if (zip_entry_open($zip, $zip_entry, 'r')) {
+            $buf = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+            fwrite($fp, "$buf");
+            
+            zip_entry_close($zip_entry);
+            fclose($fp);
         }
     }
+    zip_close($zip);
+    return true;
+} else {
+    return false;
+}
+}
 
-	function facetly_save_post($post_id) {
-		$post = get_post($post_id);
-		$post_type = $post->post_type;
-		$post_status = $post->post_status;
-		
-		if($post_status == 'trash' || $post_status == 'auto-draft' || $post_type != 'wpsc-product'){
-			return $post_id;
-		} else if ($post_status == 'publish' && $post_type == 'wpsc-product') {
-			try {
-				facetly_insert_product($post_id);
-			} catch (Exception $e) {
-				echo '
-					<tr>
-						<td colspan="11" class="custom_error">'. $e->getMessage(). '</td>
-					</tr>
-				';
-			}
-		} else if ( ($post_status == 'pending' || $post_status == 'draft' ) && $post_type == 'wpsc-product') {
+function facetly_save_post($post_id) {
+	$post = get_post($post_id);
+	$post_type = $post->post_type;
+	if (!empty($post->ancestors[0])) {
+		$ancestors = $post->ancestors[0];
+		$post_type = get_post_type($ancestors);
+	}
+	$post_status = $post->post_status;
+	$facetly_fields = get_option('facetly_fields');
+	if($post_status == 'trash' || $post_status == 'auto-draft'){	
+		return $post_id;
+	} else if ($post_status == 'publish' && in_array($post_type, $facetly_fields['node_type']) ) {
+		try {
+			facetly_insert_product($post);
+		} catch (Exception $e) {
+			echo '
+				<tr>
+					<td colspan="11" class="custom_error">'. $e->getMessage(). '</td>
+				</tr>
+			';
+		}
+	} else if ( ($post_status == 'pending' || $post_status == 'draft' ) && in_array($post_type, $facetly_fields['node_type']) ) {
+		$facetly->productDelete($post_id);
+	}
+}
+
+function facetly_insert_product($post){
+	global $wpdb;
+
+	$post_id = $post->ID;
+	//$post = get_post($post_id);
+	$url = get_permalink($post_id);
+	$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-size' );
+	$imageurl = $image['0'];
+	
+	$category = custom_taxonomies_terms_links($post);
+	$item = array();
+	$meta = get_post_meta($post_id, '');
+
+	$facetly_fields = get_option('facetly_fields');
+	foreach( $facetly_fields as $key => $value ) {
+		if (is_array($value)) {
+			continue;
+		}
+		if ( strstr($value, 'post') ) {	
+			$item[$key] = $post->$value;
+		} else if (isset($meta[$value])) {
+			$item[$key] = $meta[$value][0];
+		}
+	}
+	$item['id'] = $post_id;
+	$item['url'] = $url;
+	$item['imageurl'] = $imageurl;
+	$item['category'] = $category;
+	$date = new DateTime($item['created']);
+	$item['created'] = $date->getTimestamp() *1000;
+
+	/*if (empty($item['price'])) {
+		$item['price'] = 0;
+	}*/
+	
+	$facetly = facetly_api_init();
+	$facetly->productUpdate($item);
+	//print_r($facetly->productUpdate($item));
+	//exit();
+}
+add_action('wp_insert_post', 'facetly_save_post');
+
+function facetly_delete_product($post_id) {
+	$post = get_post($post_id);
+	$post_type = $post->post_type;
+	if (!empty($post->ancestors[0])) {
+		$ancestors = $post->ancestors[0];
+		$post_type = get_post_type($ancestors);
+	}
+	$post_type = $post->post_type;
+	$facetly_fields = get_option('facetly_fields');
+	if($post->post_status == 'trash' || $post->post_status == 'auto-draft'){
+		return $post_id;
+	} else if ( in_array($post_type, $facetly_fields['node_type']) ) {
+		try {
+			$facetly = facetly_api_init();
 			$facetly->productDelete($post_id);
+		} catch (Exception $e) {
+			echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
 		}
 	}
-
-	function facetly_insert_product($post_id){
-		global $wpdb;
-
-		$post = get_post($post_id);
-		$url = get_permalink($post_id);
-		$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full-size' );
-		$imageurl = $image['0'];
-		
-		$category = custom_taxonomies_terms_links($post_id);
-		$item = array();
-		$meta = get_post_meta($post_id, '');
-
-		$facetly_fields = get_option('facetly_fields');
-		foreach( $facetly_fields as $key => $value ) {
-			if ( strstr($value, 'post') ) {	
-				$item[$key] = $post->$value;
-			} else if (isset($meta[$value])) {
-				$item[$key] = $meta[$value][0];
-			}
-		}
-		$item['id'] = $post_id;
-		$item['url'] = $url;
-		$item['imageurl'] = $imageurl;
-		$item['category'] = $category;
-		$date = new DateTime($item['created']);
-		$item['created'] = $date->getTimestamp() *1000;
-		
-		$facetly = facetly_api_init();
-		$facetly->productUpdate($item);
-	}
-	add_action('wp_insert_post', 'facetly_save_post');
-
-	function facetly_delete_product($post_id) {
-		$post = get_post($post_id);
-		$post_type = $post->post_type;
-		if($post->post_status == 'trash' or $post->post_status == 'auto-draft' or $post_type != 'product'){
-			return $post_id;
-		} else if ($post_type == 'product') {
-			try {
-				$facetly = facetly_api_init();
-				$facetly->productDelete($post_id);
-			} catch (Exception $e) {
-				echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
-			}
-		}
-	}
-	add_action('wp_trash_post', 'facetly_delete_product');
+}
+add_action('wp_trash_post', 'facetly_delete_product');
--- facetly_reindex.php
+++ facetly_reindex.php
@@ -1,115 +1,124 @@
 <?php
-	global $percentage;
+global $percentage;
 
-	function facetly_reindex_display_error($error_message = "") {
-		return new WP_Error('product_insert', $error_message);
-	}
+function facetly_reindex_display_error($error_message = "") {
+	return new WP_Error('product_insert', $error_message);
+}
 
-	function facetly_reindex(){
-		$facetly = facetly_api_init();
+function facetly_reindex(){
+	$facetly = facetly_api_init();
 
-		global $wpdb;
-		global $total_all;
+	global $wpdb;
+	global $total_all;
 
-	    $limit = 25;
-		if ( !empty($_GET['reindex']) && $_GET['reindex'] == "y" ) {
-			$query_product = "select pt.ID, pt.post_title from ". $wpdb->prefix. "posts As pt  ";
-			$query_product .= "LEFT JOIN ". $wpdb->prefix. "postmeta As mt ON pt.ID=mt.post_id  ";
-			$query_product .= "WHERE pt.post_type = 'wpsc-product' ";
-			$query_product .= "AND pt.post_status = 'publish' ";
-			$query_product .= "GROUP BY mt.post_id ";
-			$query_product .= "ORDER BY pt.post_title ";
-			$query = $wpdb->query($query_product);
-			$total_all = $wpdb->num_rows;
-			
-			$counter = $_GET['counter'];
-			$next = $counter+1;
-			
-			$start = $counter*$limit;
-			if ($counter == 0 ) {
-				$start = 0;
-				$facetly->productTruncate();
-			}
-			$percentage = $start/$total_all*100;
-			if ($percentage < 100) {
-				$query_product .= "LIMIT ". $start. ",". $limit;
-				
-				$query = $wpdb->query($query_product);
-				$total = $wpdb->num_rows;
-				
-				$myrows = $wpdb->get_results($query_product);
-				$reindex = "y";
-				
-				foreach( $myrows as $row ) {
-					$post_id = $row->ID;
-					try {
-						facetly_insert_product($post_id);
-					} 
-					catch (Exception $e) {
-						$reindex = "n";
-						$header = '&reindex='.$reindex;
-						
-						$error_message = $e->getMessage();
-						update_option('facetly_error', $error_message);
-					}
-				}
-				$header = '&reindex='.$reindex.'&counter='.$next;
-			} else {
-				$reindex = "n";
-				$header = '&reindex='.$reindex;
-				$percentage = 100;
-				$completed = true;
-			}
-			$error_message = get_option('facetly_error');
-			if (!empty($error_message)) {
-				$reindex = "n";
-				$header = '&reindex='.$reindex;
-				$percentage = 100;
-				$completed = true;
-			}
-			
-			echo '<meta http-equiv="refresh" content="2;admin.php?page=facetly-configuration-reindex'. $header. '"/>';
-			echo '<meta http-equiv="cache-control" content="NO-CACHE"/>';
-			echo "<h2>" . __( 'Reindexing All WP e-Commerce Data' ) . "</h2>";
-			echo "<h4>" . __( 'Please Wait Until Finish' ) . "</h4>";
-			echo do_shortcode("[facetly_progress percentage=". $percentage. "]");
+    $limit = 25;
+	if ( !empty($_GET['reindex']) && $_GET['reindex'] == "y" ) {
+		$facetly_fields = get_option('facetly_fields');
+		$node_types = $facetly_fields['node_type'];
+		$node_type = array();
+		foreach ($node_types as $key => $value) {
+			$node_type[] = "pt.post_type = ". "'". $value. "'";
 		}
-		if ( !isset($_GET['reindex']) || $_GET['reindex'] == "n" ) {
-			if ( !empty($_GET['reindex']) && $_GET['reindex'] == "n" && empty($percentage) ) {
-				$error_message = get_option('facetly_error');
-				$error = facetly_reindex_display_error($error_message);
-				delete_option('facetly_error');
-				if (!empty($error_message)) {
-					if ( is_wp_error($error) ) echo '<div class="error"><p><strong>'. $error->get_error_message(). '</strong></p></div>';
-				} else {
-					echo "<div class='custom_notice'><p><strong>" . __( 'Reindex Completed' ) . "</strong></p></div>";
-				}
-			}
-			$get = $_GET;
-			$get['counter'] = 0;
-			$get['reindex'] = 'y';
+		$post_type = implode(' OR ', $node_type);
+
+		$query_product = " select pt.ID, pt.post_title from ". $wpdb->prefix. "posts As pt  ";
+		$query_product .= " LEFT JOIN ". $wpdb->prefix. "postmeta As mt ON pt.ID=mt.post_id WHERE ";
+		$query_product .= "(". $post_type.")";
+		$query_product .= " AND pt.post_status = 'publish' ";
+		$query_product .= " GROUP BY mt.post_id ";
+		$query_product .= " ORDER BY pt.post_title ";
+		$query = $wpdb->query($query_product);
+		$total_all = $wpdb->num_rows;
+		
+		$counter = $_GET['counter'];
+		$next = $counter+1;
+		
+		$start = $counter*$limit;
+		if ($counter == 0 ) {
+			$start = 0;
+			$facetly->productTruncate();
+		}
+		$percentage = $start/$total_all*100;
+		if ($percentage < 100) {
+			$query_product .= "LIMIT ". $start. ",". $limit;
 			
-			$url_query = http_build_query($get,'','&');
-			try {
-				$fields = $facetly->fieldSelect();	
-				if (!empty($fields)) {
-				?>
-					<div class="wrap">
-						<?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
-						<?php echo "<h4>" . __( 'Facetly Reindex' ) . "</h4>"; ?>  
-						<form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['PHP_SELF']). "?". $url_query; ?>">  	
-							<p class="submit">  
-								<input type="submit" name="Submit" value="<?php _e('Start Reindex' ) ?>" />  
-							</p>  
-						</form>  
-					</div> 
-			<?php
-				}
-			} catch (Exception $e) {
-				echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
-				if (empty($fields)) {
-					echo '<div class="error"><p><strong>Can not connect to server, please check your consumer API configuration or contact our support if problem persist.</strong></p></div>';
+			$query = $wpdb->query($query_product);
+			$total = $wpdb->num_rows;
+			
+			$myrows = $wpdb->get_results($query_product);
+			$reindex = "y";
+			
+			foreach( $myrows as $row ) {
+				$post_id = $row->ID;
+				$post = get_post($post_id);
+				try {
+					facetly_insert_product($post);
+				} 
+				catch (Exception $e) {
+					$reindex = "n";
+					$header = '&reindex='.$reindex;
+					
+					$error_message = $e->getMessage();
+					update_option('facetly_error', $error_message);
 				}
 			}
-		} 	
+			$header = '&reindex='.$reindex.'&counter='.$next;
+		} else {
+			$reindex = "n";
+			$header = '&reindex='.$reindex;
+			$percentage = 100;
+			$completed = true;
+		}
+		$error_message = get_option('facetly_error');
+		if (!empty($error_message)) {
+			$reindex = "n";
+			$header = '&reindex='.$reindex;
+			$percentage = 100;
+			$completed = true;
+		}
+		
+		echo '<meta http-equiv="refresh" content="2;admin.php?page=facetly-configuration-reindex'. $header. '"/>';
+		echo '<meta http-equiv="cache-control" content="NO-CACHE"/>';
+		echo "<h2>" . __( 'Reindexing All WP e-Commerce Data' ) . "</h2>";
+		echo "<h4>" . __( 'Please Wait Until Finish' ) . "</h4>";
+		echo do_shortcode("[facetly_progress percentage=". $percentage. "]");
 	}
+	if ( !isset($_GET['reindex']) || $_GET['reindex'] == "n" ) {
+		if ( !empty($_GET['reindex']) && $_GET['reindex'] == "n" && empty($percentage) ) {
+			$error_message = get_option('facetly_error');
+			$error = facetly_reindex_display_error($error_message);
+			delete_option('facetly_error');
+			if (!empty($error_message)) {
+				if ( is_wp_error($error) ) echo '<div class="error"><p><strong>'. $error->get_error_message(). '</strong></p></div>';
+			} else {
+				echo "<div class='custom_notice'><p><strong>" . __( 'Reindex Completed' ) . "</strong></p></div>";
+			}
+		}
+		$get = $_GET;
+		$get['counter'] = 0;
+		$get['reindex'] = 'y';
+		
+		$url_query = http_build_query($get,'','&');
+		try {
+			$fields = $facetly->fieldSelect();	
+			if (!empty($fields)) {
+			?>
+				<div class="wrap">
+					<?php    echo "<h2>" . __( 'Facetly Configuration' ) . "</h2>"; ?>  
+					<?php echo "<h4>" . __( 'Facetly Reindex' ) . "</h4>"; ?>  
+					<form name="facetly_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['PHP_SELF']). "?". $url_query; ?>">  	
+						<p class="submit">  
+							<input type="submit" name="Submit" value="<?php _e('Start Reindex' ) ?>" />  
+						</p>  
+					</form>  
+				</div> 
+		<?php
+			}
+		} catch (Exception $e) {
+			echo '<div class="error"><p><strong>'. $e->getMessage(). '</strong></p></div>';
+			if (empty($fields)) {
+				echo '<div class="error"><p><strong>Can not connect to server, please check your consumer API configuration or contact our support if problem persist.</strong></p></div>';
+			}
+		}
+	} 	
+}
